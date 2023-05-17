9015e723a068f6ff74b3e0751a2331669ac4d26b
amipatel-gitthub <amipatel-gitthub@gmail.com>
2023-05-17 14:36:20 +0530

    first commit

--- src/components/Fourth_Task.jsx
+++ src/components/Fourth_Task.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { useState , useEffect , useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import { Link } from 'react-router-dom'
 
-function Fourth_Task(){
+function Fourth_Task() {
 
     const [user, setUser] = React.useState(0);
     const count = React.useRef(0)
 
-    useEffect(() =>{
+    useEffect(() => {
         count.current += 1;
     })
-    return(
+    return (
         <>
+
+            <div class="d-flex justify-content-center mt-5 mb-5">
+                <Link to='/' className="text-primary fw-bold ms-3 me-3"><i class="bi bi-1-circle-fill"></i> First Task</Link>
+                <Link to='/Second_Task' className="text-warning fw-bold  ms-3 me-3"><i class="bi bi-2-circle-fill"></i> Second Task</Link>
+                <Link to='/Third_Task' className="text-info fw-bold  ms-3 me-3"><i class="bi bi-3-circle-fill"></i> Third Task</Link>
+                <Link to='/Fourth_Task' className="text-secondary fw-bold  ms-3 me-3"><i class="bi bi-4-circle-fill"></i> Fourth Task</Link>
+            </div>
             <div className="text-center">
                 <input type="text"
-                        onChange={(e) => setUser(e.target.value)}
+                    onChange={(e) => setUser(e.target.value)}
                 /><br></br><br></br>
                 <p>You're input Text is.. <b><i>{user}</i></b> </p>
 
                 <h6>You're Data was Render {count.current} Times.</h6>
+            </div>
+
+            <div className="mt-5 text-center text-secondary">
+                <p>[ useRef example ]</p>
             </div>
         </>
     );
